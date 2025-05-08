91e6db2bdaf112bf4bc2e81e1f450770c92171cc
LORDx0001 <bexruztoj2005@gmail.com>
2025-05-08 13:37:45 +0500

    commit v.4

--- tiler_app/views.py
+++ tiler_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Service, Project, Testimonial
 from .forms import ContactForm
-
+import requests
 def home(request):
     services = Service.objects.all()[:3]  # Get first 3 services
     projects = Project.objects.all().order_by('-date_completed')[:8]  # Get latest 8 projects
@@ -40,9 +40,24 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            sender(form.cleaned_data)
             messages.success(request, 'Your message has been sent. We will contact you soon!')
             return redirect('contact')
     else:
         form = ContactForm()
     
     return render(request, 'tiler_app/contact.html', {'form': form})
+
+bot_token = '7939886196:AAHOJhfrXpaKNjHoRl7MuKUPGVDR1LZ28Ak'
+url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+
+def sender(data):
+    text = ""
+    for key, value in data.items():
+        text += f'{key}: {value}\n'
+    params = {
+        "chat_id": -4630075533,
+        "text": text,
+    }
+    response = requests.post(url, params=params)
+    return response.status_code == 200
